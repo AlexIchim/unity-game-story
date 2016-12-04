5f93cf5470190670b885e25a1f1b73e7e898a708
Alexandru Ichim <alexichim94@yahoo.ro>
2016-12-04 19:42:11 +0200

    Added palyer horizontal movement, adn object collision detection.

--- Assets/Code/CharacterController2D.cs
+++ Assets/Code/CharacterController2D.cs
@@ -13,49 +13,160 @@ public class CharacterController2D : MonoBehaviour {
     public ControllerParameters2D DefaultParameters;
 
     public ControllerState2D State { get; private set; }
+    public Vector2 Velocity { get { return _velocity; } }
+    public bool CanJump { get { return false; } }
+    private bool HandleCollisions { get; set; }
+    public ControllerParameters2D Parameters { get { return _overrideParameters ?? DefaultParameters; } }
+
+    private Vector2 _velocity;
+    private Transform _transform;
+    private Vector3 _localScale;
+    private BoxCollider2D _boxCollider;
+    private ControllerParameters2D _overrideParameters;
+    private Vector3 
+        _raycastTopLeft,
+        _raycastBottomLeft,
+        _raycastBottomRight;
+
+    private float
+        _verticalDistanceBetweenRays,
+        _horizontalDistanceBetweenRays;
 
     public void Awake()
     {
+        State = new ControllerState2D();
+        HandleCollisions = true;
+        
+        _transform = transform;
+        _localScale = transform.localScale;
+        _boxCollider = GetComponent<BoxCollider2D>();
+
+        var colliderWidth = _boxCollider.size.x * Mathf.Abs(transform.localScale.x) - (2 * SkinWidth);
+        _horizontalDistanceBetweenRays = colliderWidth / (TotalVerticalRays - 1);
+
+        var colliderHeight = _boxCollider.size.y * Mathf.Abs(transform.localScale.y) - (2 * SkinWidth);
+        _verticalDistanceBetweenRays = colliderHeight / (TotalHorizontalRays - 1);
     }
 
     public void AddForce(Vector2 force)
     {
+        _velocity = force;
     }
 
     public void SetForce(Vector2 force)
     {
+        _velocity = force;
     }
 
     public void SetHorizontalForce(float x)
     {
+        _velocity.x = x;
     }
 
-    public void SetVerticalForce(float x)
+    public void SetVerticalForce(float y)
     {
+        _velocity.y = y;
     }
 
     public void Jump()
     {
     }
 
+    /* Invoked after the update of all other objects, move character based on Velocity scaled by time. 5 units to Right * time */
     public void LateUpdate()
     {
+        Move(Velocity * Time.deltaTime);
     }
 
     private void Move(Vector2 deltaMovement)
     {
+        var wasGrounded = State.IsCollidingBelow;
+        State.Reset(); // Set all booleans to false and angle to 0
+        if (HandleCollisions)
+        {
+            HandlePlatforms();
+            CalculateRayOrigins(); // every time we move ray origins are changed
+
+            if (deltaMovement.y < 0 && wasGrounded) // they are moving down
+                HandleVerticalSlope(ref deltaMovement);
+
+            if (Mathf.Abs(deltaMovement.x) > .001f)
+                MoveHorizontally(ref deltaMovement);
+
+            MoveVertically(ref deltaMovement);
+        }
+
+        _transform.Translate(deltaMovement, Space.World); // move the player to the next position after checking the previous conditions
+
+
+        //TODO: Addition moving platform code
+        if (Time.deltaTime > 0)
+            _velocity = deltaMovement / Time.deltaTime;
+
+        _velocity.x = Mathf.Min(_velocity.x, Parameters.MaxVelocity.x);
+        _velocity.y = Mathf.Min(_velocity.y, Parameters.MaxVelocity.y);
+
+        if (State.IsMovingUpSlope)
+            _velocity.y = 0;
     }
 
-    private void HandlePlatform()
+    private void HandlePlatforms()
     {
     }
 
+    /* 3 potential ray cast origins of the box, 3 vectors to cast rays*/
     private void CalculateRayOrigins()
     {
+        var size = new Vector2(_boxCollider.size.x * Mathf.Abs(_localScale.x), _boxCollider.size.y * Mathf.Abs(_localScale.y));
+        var center = new Vector2(_boxCollider.offset.x * _localScale.x, _boxCollider.offset.y * _localScale.y);
+
+        _raycastTopLeft = _transform.position + new Vector3(center.x - size.x + SkinWidth, center.y + size.y - SkinWidth);
+        _raycastBottomRight = _transform.position + new Vector3(center.x + size.x - SkinWidth, center.y - size.y + SkinWidth);
+        _raycastBottomLeft = _transform.position + new Vector3(center.x - size.x + SkinWidth, center.y - size.y + SkinWidth);
+
     }
 
     private void MoveHorizontally(ref Vector2 deltaMovement)
     {
+        var isGoingRight = deltaMovement.x > 0;                                         // We are going right
+        var rayDistance = Mathf.Abs(deltaMovement.x) + SkinWidth;                       //Distance from player to destination target
+        var rayDirection = isGoingRight ? Vector2.right : -Vector2.right;               //Ray direction unit vector
+        var rayOrigin = isGoingRight ? _raycastBottomRight : _raycastBottomLeft;        //Depends on direction
+
+        for (var i = 0; i < TotalHorizontalRays; i++)   //Cast rays to target
+        {
+            var rayVector = new Vector2(rayOrigin.x, rayOrigin.y + (i * _verticalDistanceBetweenRays));
+            Debug.DrawRay(rayVector, rayDirection * rayDistance, Color.red);
+
+            var rayCastHit = Physics2D.Raycast(rayVector, rayDirection, rayDistance, PlatformMask);     // draw line for each raycast
+            if (!rayCastHit)    //check if hit something
+                continue;
+
+            if (i == 0 && HandleHorizontalSlope(ref deltaMovement, Vector2.Angle(rayCastHit.normal, Vector2.up), isGoingRight))
+                break;
+
+            /* if we hit soemthing than we can move that far forward*/
+            deltaMovement.x = rayCastHit.point.x - rayVector.x;
+            rayDistance = Mathf.Abs(deltaMovement.x);
+
+
+            /* subtract skinwidth error, shooting ray from inside the object */
+            if (isGoingRight)
+            {
+                deltaMovement.x -= SkinWidth;
+                State.IsCollidingRight = true;
+            }
+            else
+            {
+                deltaMovement.x += SkinWidth;
+                State.IsCollidingLeft = true;
+            }
+
+            if (rayDistance < SkinWidth + .0001f)
+                break;
+
+        }
+
     }
 
     private void MoveVertically(ref Vector2 deltaMovement)
@@ -66,8 +177,9 @@ public class CharacterController2D : MonoBehaviour {
     {
     }
 
-    private void HandleHorizontalSlope(ref Vector2 deltaMovement, float angle, bool isGoingRight)
+    private bool HandleHorizontalSlope(ref Vector2 deltaMovement, float angle, bool isGoingRight)
     {
+        return false;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
--- Assets/Code/ControllerParameters2D.cs
+++ Assets/Code/ControllerParameters2D.cs
@@ -1,24 +1,27 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
-using System;
 
 [Serializable]
-public class ControllerParameters2D {
+public class ControllerParameters2D
+{
+    public enum JumpBehavior
+    {
+        CanJumpOnGround,
+        CanJumpAnywhere,
+        CantJump
+    }
 
-	public enum JumpBehavior {
-		CanJumpOnGround,
-		CanJumpAnywhere,
-		CantJump
-	}
+    public Vector2 MaxVelocity = new Vector2(float.MaxValue, float.MaxValue);
 
-	public Vector2 MaxVelocity = new Vector2(float.MaxValue, float.MaxValue);
+    [Range(0, 90)]
+    public float SlopeLimit = 30;
 
-	[Range(0, 90)]
-	public float SlopeLimit = 30;
+    public float Gravity = -25f;
 
-	public float Gravity = -25f;
+    public JumpBehavior JumpRestrictions;
 
-	public JumpBehavior JumpRestrictions;
+    public float JumpFrequency = .25f;
 
-	public float JumpFrequency = .25f;
+    public float JumpMagnitude = 12;
 }
